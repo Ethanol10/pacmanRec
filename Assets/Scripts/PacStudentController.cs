2e17c16c78a12ea04c98bdf3f6ecfab808a65eca
Ethan Goh <ethangoh98@gmail.com>
2020-10-07 01:10:04 +1100

    Added movement to pacman

---
+++ Assets/Scripts/PacStudentController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacStudentController : MonoBehaviour
+{
+    private Tweener tweener;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tweener = GetComponent<Tweener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("w")){
+            if(!tweener.tweenActive()){
+                // lastKey = 'w';
+                tweener.AddTween(gameObject.transform, 
+                                gameObject.transform.position, 
+                                new Vector3(gameObject.transform.position.x,gameObject.transform.position.y + 1.25f, 0), 
+                                0.7f);
+            }
+        }
+        if(Input.GetKey("a")){
+            if(!tweener.tweenActive()){
+                //lastKey = 'a';
+                tweener.AddTween(gameObject.transform, 
+                                gameObject.transform.position, 
+                                new Vector3(gameObject.transform.position.x - 1.25f ,gameObject.transform.position.y, 0), 
+                                0.7f);
+            }
+        }
+        if(Input.GetKey("s")){
+            if(!tweener.tweenActive()){
+                //lastKey = 's';
+                tweener.AddTween(gameObject.transform, 
+                                gameObject.transform.position, 
+                                new Vector3(gameObject.transform.position.x,gameObject.transform.position.y - 1.25f, 0), 
+                                0.7f);
+            }
+        }
+        if(Input.GetKey("d")){
+            if(!tweener.tweenActive()){
+                //lastKey = 'd';
+                tweener.AddTween(gameObject.transform, 
+                                gameObject.transform.position, 
+                                new Vector3(gameObject.transform.position.x + 1.25f ,gameObject.transform.position.y, 0), 
+                                0.7f);
+            }
+        }
+    }
+}
--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 
 public class Tweener : MonoBehaviour
 {
-    private List<Tween> activeTweens = new List<Tween>();
+    private Tween activeTween;
 
     void Start(){
-
+        activeTween = null;
     }
 
     void Update(){
         float currentTime = Time.time;
+
         // for(int i = 0; i < activeTweens.Count; i++){     
         //     Debug.Log(i);
         //     if(Vector3.Distance(activeTweens[i].Target.transform.position, activeTweens[i].EndPos) > 0.1f){
@@ -25,37 +26,39 @@ public class Tweener : MonoBehaviour
         //         activeTweens.RemoveAt(i);
         //     }
         // }
-        if(Vector3.Distance(activeTweens[0].Target.position, activeTweens[0].EndPos) > 0.1f){
-            // float x = (currentTime - activeTweens[0].StartTime)/activeTweens[0].Duration;
-            // float timeFraction = 1 - Mathf.Cos((x * Mathf.PI) / 2);
-            float timeFraction = (currentTime - activeTweens[0].StartTime)/activeTweens[0].Duration;
-            activeTweens[0].Target.transform.position = Vector3.Lerp(activeTweens[0].StartPos, activeTweens[0].EndPos, timeFraction);          
-        }
-        else if(Vector3.Distance(activeTweens[0].Target.position, activeTweens[0].EndPos) <= 0.1f){
-            Debug.Log(activeTweens[0].Target);
-            activeTweens[0].Target.position = activeTweens[0].EndPos;
-            activeTweens.RemoveAt(0);
+        if(activeTween != null){     
+            if(Vector3.Distance(activeTween.Target.position, activeTween.EndPos) > 0.01f){
+                //float x = (currentTime - activeTween.StartTime)/activeTween.Duration;
+                //float timeFraction = 1 - Mathf.Cos((x * Mathf.PI) / 2);
+                float timeFraction = (currentTime - activeTween.StartTime)/activeTween.Duration;
+                activeTween.Target.transform.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);          
+            }
+            else if(Vector3.Distance(activeTween.Target.position, activeTween.EndPos) <= 0.01f){
+                //Debug.Log(activeTweens[0].Target);
+                activeTween.Target.position = activeTween.EndPos;
+                activeTween = null;
+            }
         }
     }
 
-    public bool AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration, float waitMod){
+    public bool AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration){
         // if(!TweenExists(targetObject)){
-            activeTweens.Add(new Tween(targetObject, startPos, endPos, Time.time + waitMod, duration));
-            return true;
+        activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+        return true;
         // }
         // return false;
     }
 
-    public bool TweenExists(Transform target){
-        for(int i = 0; i < activeTweens.Count; i++){
-            if(activeTweens[i].Target == target){
-                return true;
-            }
-        }
-        return false;
-    }
+    //public bool TweenExists(Transform target){
+        // for(int i = 0; i < activeTweens.Count; i++){
+        //     if(activeTweens[i].Target == target){
+        //         return true;
+        //     }
+        // }
+        // return false;
+    //}
 
-    public int noOfActiveTweens(){
-        return activeTweens.Count;
+    public bool tweenActive(){
+        return (activeTween != null);
     }
 }
